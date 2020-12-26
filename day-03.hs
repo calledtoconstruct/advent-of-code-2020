81f7b08d6367752782f344469284b7f5f73921de
Joseph Woolley <joe@calledtoconstruct.net>
2020-12-26 23:13:55 +0000

    Solved part two day three.

--- day-03.hs
+++ day-03.hs
@@ -1,6 +1,8 @@
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 
+-- Part One
+
 examplePartOne :: [[Char]]
 examplePartOne = [
     "..##.......",
@@ -35,12 +37,12 @@ get :: (Int, Int) -> [[Char]] -> Char
 get (currentX, currentY) = getColumn currentX . getRow currentY
 
 done :: [[Char]] -> (Int, Int) -> Bool
-done board (_, currentY) = currentY == length board
+done board (_, currentY) = currentY >= length board
 
-traversePartOne :: [[Char]] -> (Int, Int) -> (Int, Int) -> Int -> Int
-traversePartOne board currentLocation stepToTake currentNumberOfTrees
-    | done board currentLocation = nextNumberOfTrees
-    | otherwise = traversePartOne board nextLocation stepToTake nextNumberOfTrees
+traverseBoard :: [[Char]] -> (Int, Int) -> (Int, Int) -> Int -> Int
+traverseBoard board currentLocation stepToTake currentNumberOfTrees
+    | done board currentLocation = currentNumberOfTrees
+    | otherwise = traverseBoard board nextLocation stepToTake nextNumberOfTrees
     where nextLocation = step currentLocation stepToTake $ width board
           onTree = '#' == get currentLocation board
           nextNumberOfTrees = currentNumberOfTrees + if onTree then 1 else 0
@@ -51,4 +53,26 @@ readActual = fmap Text.unpack . Text.lines <$> Text.readFile "data-day-03.txt"
 traversePartOneActual :: IO Int 
 traversePartOneActual = do
     board <- readActual
-    return $ traversePartOne board (0, 0) (3, 1) 0
+    return $ traverseBoard board (0, 0) (3, 1) 0
+
+-- Part Two
+
+partTwoExampleAnswer :: Int
+partTwoExampleAnswer = 336
+
+slopes :: [(Int, Int)]
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+    ]
+
+traverseEach :: [[Char]] -> [(Int, Int)] -> [Int]
+traverseEach board = fmap (\slope -> traverseBoard board (0, 0) slope 0)
+
+traversePartTwoActual :: IO Int 
+traversePartTwoActual = do
+    board <- readActual
+    return $ product $ traverseEach board slopes
