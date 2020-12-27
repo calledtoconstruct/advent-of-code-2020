5f4e224ad9daedc2a6a81cb8338cf26d256dd079
Joseph Woolley <joe@calledtoconstruct.net>
2020-12-27 20:38:56 +0000

    Solved part two of day six.

--- src/day-06.hs
+++ src/day-06.hs
@@ -1,19 +1,31 @@
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+import qualified Data.List as List (sort)
 import qualified Data.List.Unique as List (sortUniq)
 
 data Group = Group {
-    answers :: Text.Text
+    answers :: Text.Text,
+    members :: Int,
+    unanimous :: Text.Text
 } deriving (Show)
 
 collapse :: Group -> Group
-collapse group = group { answers = Text.pack $ List.sortUniq $ Text.unpack $ answers group }
+collapse group = group {
+    answers = Text.pack $ List.sortUniq $ Text.unpack $ answers group
+}
 
 newGroup :: Group
-newGroup = Group { answers = Text.pack "" }
+newGroup = Group {
+    answers = Text.pack "",
+    members = 0,
+    unanimous = Text.pack ""
+}
 
 appendAnswer :: Group -> Text.Text -> Group
-appendAnswer group response = group { answers = flip Text.append response $ answers group }
+appendAnswer group response = group {
+    answers = flip Text.append response $ answers group,
+    members = (+) 1 $ members group
+}
 
 loadAnswers :: [Group] -> [Text.Text] -> [Group]
 loadAnswers groups [] = groups
@@ -26,6 +38,11 @@ loadAnswers groups responses
 
 sumAnswers :: Int -> [Group] -> Int 
 sumAnswers = foldl (\ sum currentGroup -> (+) sum $ Text.length $ answers currentGroup)
+
+loadData :: IO [Text.Text]
+loadData = Text.lines <$> Text.readFile "./data/data-day-06.txt"
+
+-- Part One
 
 sampleAnswers :: [Text.Text]
 sampleAnswers = [
@@ -46,10 +63,29 @@ sampleAnswers = [
     Text.pack "b"
     ]
 
-loadData :: IO [Text.Text]
-loadData = Text.lines <$> Text.readFile "./data/data-day-06.txt"
-
 partOne :: IO Int 
 partOne = do
     responses <- loadData
     return $ sumAnswers 0 $ collapse <$> loadAnswers [newGroup] responses
+
+-- Part Two
+
+x = Text.group $ Text.pack $ List.sort "abacdedeff"
+
+removeNonUnanimous :: Group -> Group
+removeNonUnanimous group
+    | null filtered = group { unanimous = Text.pack "" }
+    | otherwise = group {
+        unanimous = Text.pack $ List.sortUniq $ Text.unpack $ foldl1 Text.append filtered
+    }
+    where grouped = Text.group $ Text.pack $ List.sort $ Text.unpack $ answers group
+          filtered = filter (\ x -> Text.length x == members group) grouped
+
+sumUnanimousAnswers :: Int -> [Group] -> Int 
+sumUnanimousAnswers = foldl (\ sum currentGroup -> (+) sum $ Text.length $ unanimous currentGroup)
+
+partTwo :: IO Int 
+partTwo = do
+    responses <- loadData
+    return $ sumUnanimousAnswers 0 $ removeNonUnanimous <$> loadAnswers [newGroup] responses
+
